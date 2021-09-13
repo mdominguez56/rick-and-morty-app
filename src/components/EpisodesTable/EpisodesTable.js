b0ab108ba6c0a9bafe369bd7a1d3a782975a54f6
Matias Dominguez Alonso <matiasdominguez3@gmail.com>
2021-09-13 11:19:03 -0300

    add button see all

--- src/components/EpisodesTable/EpisodesTable.js
+++ src/components/EpisodesTable/EpisodesTable.js
@@ -8,6 +8,7 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
 
 const StyledTableCell = withStyles((theme) => ({
   head: {
@@ -40,12 +41,10 @@ export default function EpisodesTable() {
     const fetchData = async () => {
       try {
         const response = await Axios({
-          url: "https://rickandmortyapi.com/api/episode",
-          url2: "https://rickandmortyapi.com/api/episode?page=2",
+          url: "https://rickandmortyapi.com/api/episode/1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41",
         });
 
-        setEpisodes(response.data.results);
-        console.log(response.data.results);
+        setEpisodes(response.data);
       } catch (error) {
         console.log(error);
       }
@@ -76,7 +75,9 @@ export default function EpisodesTable() {
               <StyledTableCell>{episode.name}</StyledTableCell>
               <StyledTableCell>{episode.air_date}</StyledTableCell>
               <StyledTableCell>
-                <button>See all</button>
+                <Button variant="contained" color="primary">
+                  See all
+                </Button>
               </StyledTableCell>
             </StyledTableRow>
           ))}
